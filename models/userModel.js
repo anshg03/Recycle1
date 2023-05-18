c5d3b2e9c950b1d814572fd17e160dc4387e6b00
Anugrah0515 <111530640+Anugrah0515@users.noreply.github.com>
2023-05-19 02:27:31 +0530

    userModule updated
    added the email and password section schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,40 @@
-const moongoose =require('moongoose');
+const moongoose = require('moongoose');
 
-const userSchema= new mongoose.Schema({
-    name:{
-        type:String ,
-        required: [true , "A name is required"],
-        unique: true, 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A name is required"],
+        unique: true,
     },
-    description :{
-        type:String,
-        trim:true
+    description: {
+        type: String,
+        trim: true
     },
-    proImage:{
-        type:String,
-        reqired:[true , "Image is required"]
+    proImage: {
+        type: String,
+        reqired: [true, "Image is required"]
     },
-    creationTime:{
-        type:Date,
-        default : Date.now()
+    creationTime: {
+        type: Date,
+        default: Date.now()
     },
-   
+    email: {
+        type: String,
+        reqired: [true, "Email is required"],
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: function (value) {
+                return "/\S+@\S+\.\S+/".test(value);
+            },
+            message: 'Invalid email address',
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-const USER = mongoose.model("USER",userSchema); 
+const USER = mongoose.model("USER", userSchema);
 module.exports = USER;
