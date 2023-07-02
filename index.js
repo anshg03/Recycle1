4485c13372741fed46e7c2f5e9a82fd5172d2fec
anshg03 <99557054+anshg03@users.noreply.github.com>
2023-07-02 23:55:30 +0530

    Made authextractor and added rate limiter

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+const rateLimit = require("express-rate-limit");
 const bodyParser = require("body-parser");
+const authExtractor = require("./middleware/authExtractor");
 const cors = require("cors");
+const postRouter = require("./Routes/productRoute");
 
 const app = express();
 app.use(cors());
@@ -10,7 +13,16 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+const limiter = rateLimit({
+  max: 100,
+  windows: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
+
 app.use("/api", require("./Routes/userRoute"));
+app.use("/api/product", authExtractor, postRouter);
 
 //const port = process.env.PORT || 3000;
 // app.listen(port, () => {
